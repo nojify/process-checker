69319e1f24baea5a66a0e5154fc22dafce70097d
Manoj Kunthu <contact@manojkunthu.com>
2020-03-01 15:24:58 -0500

    Check for process running secondary script

--- process_chk.sh
+++ process_chk.sh
@@ -10,12 +10,13 @@ echo "Process Start - $n"
 # Check if a process for this script is already running
 
 ps -ef | grep "/bin/sh" | grep $n | grep -v $$ | grep -v "/bin/sh -c"
-if [ $? -eq 0 ]; then
-  echo "Process already running"
-  exit 0
-fi
+if [ $? -eq 0 ]; then echo "Process already running...exit"; exit 0; fi
+
+
+ps -ef | grep "/bin/sh" | grep "secondary_script.sh" | grep -v "/bin/sh -c"
+if [ $? -eq 0 ]; then echo "Secondary Script already running...exit"; exit 0; fi
 
 # ------------------------------------------ 
 
-sleep 65
+sleep 40
 echo "Done"
---
+++ secondary_script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Secondary script that just runs for 40 seconds
+#
+
+echo "Secondary Start"
+sleep 20
+echo "Secondary End"
+
